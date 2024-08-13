a21f6d1b177ece51fbfae5b1e9873315a9fd5466
bacan25 <gonzalezmiguel2000@gmaiil.com>
2024-08-13 13:29:44 -0500

    En teoría ya hay salas

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -9,6 +9,11 @@ using Photon.Realtime;
 public class GameManager : MonoBehaviourPunCallbacks
 {
     public TMP_Text textIndicator;
+    public GameObject btnConect;
+
+    private void Start() {
+        btnConect.SetActive(false);
+    }
     public void ConnectPhoton()
     {
         if(!PhotonNetwork.IsConnected)
@@ -31,10 +36,29 @@ public class GameManager : MonoBehaviourPunCallbacks
     {
         base.OnConnectedToMaster();
         textIndicator.text = "Bienvenido" + PhotonNetwork.NickName;
+        btnConect.SetActive(true);
     }
     public override void OnDisconnected(DisconnectCause cause)
     {
         base.OnDisconnected(cause);
+    }
+    public void CreateRoom()
+    {
+        string user = PhotonNetwork.NickName;
+        string nameRoom = "Sala1";
+        RoomOptions optionRoom = new RoomOptions();
+        optionRoom.IsVisible = true;
+        optionRoom.MaxPlayers = 4;
+        optionRoom.PublishUserId = true;
+
+        PhotonNetwork.JoinOrCreateRoom(nameRoom,optionRoom,TypedLobby.Default);
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("Estamos Melos sisas sisas"+PhotonNetwork.CurrentRoom.Name + "Bienvenido"+PhotonNetwork.NickName);
     }
 
 }
---
+++ Assets/scripts/WindowHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowHandler : MonoBehaviour
+{
+  public GameObject[] windows;
+  
+  public void EnabledWindow(int idWindow)
+  {
+    windows[idWindow].SetActive(true);
+
+    for (int i = 0; i<windows.Length; i++)
+    {
+        if(idWindow !=i)
+          windows[i].SetActive(false);
+
+    }
+
+  }
+
+}
